f366bf66cc9b4322818b4bb03bc6e1093f9fe493
Karen Fannin <karenfannin@rocketmortgage.com>
2022-05-12 19:20:31 -0400

    Add project files.

---
+++ Circle Lab/Circle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Lab
+{
+    class Circle
+    {
+        private double radius;
+        public double circumference;
+        public double area;
+
+        public Circle()
+        {
+            // nothing needed here
+        }
+
+        public Circle(double inRadius)
+        {
+            radius = inRadius;
+        }
+        
+        public double CalculateCircumference(double inRadius)
+        {
+            circumference  = 2 * Math.PI * inRadius;
+            return circumference;
+        }
+        public string CalculateFormattedCircumference()
+        {
+            double roundedCirc = FormatNumber(circumference);
+            return $"The circumference of your circle is: {roundedCirc}";
+        }
+        public double CalculateArea(double myRadius)
+        {
+            area = Math.PI * Math.Pow(radius, 2);
+            return area;
+        }
+        public string CalculateFormattedArea()
+        {
+            double roundedArea = FormatNumber(area);
+            return $"The area of your circle is: {roundedArea}";
+        }
+        private double FormatNumber(double inNumber)
+        {
+            return Math.Round(inNumber, 2);
+        }     
+    }
+}
---
+++ Circle Lab/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Circle_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool cont = true;
+            Console.WriteLine("Welcome to the Circle Tester.");
+
+            bool isValidDouble = true;
+            double myRadius;
+            string answer;
+            int myCount = 0;
+            do
+            {
+                Console.WriteLine("Please enter a radius:");
+                answer = Console.ReadLine();
+                isValidDouble = double.TryParse(answer, out myRadius);
+                if (isValidDouble == false)
+                {
+                    Console.WriteLine("Invalid Radius");
+                    Console.WriteLine("Please enter a radius:");
+                    answer = Console.ReadLine();
+                    isValidDouble = double.TryParse(answer, out myRadius);
+                } while (isValidDouble == false) ;
+
+                Circle myCircle = new Circle(myRadius);
+                myCount++;
+
+                myCircle.CalculateCircumference(myRadius);
+
+                Console.WriteLine($"{myCircle.CalculateFormattedCircumference()}");
+
+                myCircle.CalculateArea(myRadius);
+                Console.WriteLine($"{myCircle.CalculateFormattedArea()}");
+
+                Console.WriteLine("Do you want to continue? (y/n)");
+                answer = Console.ReadLine();
+                if (answer != "y")
+                {
+                    cont = false;
+                }
+            } while (cont == true);
+            Console.WriteLine($"You built {myCount} circles\n");
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
